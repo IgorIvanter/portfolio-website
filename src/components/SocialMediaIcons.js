ea72abae3e742499a69bd774d8e72e7a25d8a620
Turing machine <igor@Turings-Air.digi.box>
2022-12-14 15:09:11 +0100

    added working links to icons

--- src/components/SocialMediaIcons.js
+++ src/components/SocialMediaIcons.js
@@ -1,17 +1,23 @@
 import linkedInIcon from '../assets/img/nav-icon1.svg'
-import facebookIcon from '../assets/img/nav-icon2.svg'
-import instagramIcon from '../assets/img/nav-icon3.svg'
+import facebookIcon from '../assets/img/github.svg'
+import instagramIcon from '../assets/img/telegram.svg'
+
+
+const LINKEDIN_LINK = "https://www.linkedin.com/in/igor-ivanter-abab26256/"
+const GITHUB_LINK = "https://github.com/IgorIvanter"
+const TELEGRAM_LINK = "https://web.telegram.org/k/#@igor_ivanter"
+
 
 function SocialMediaIcons() {
     return (
         <div className="social-icon">
-            <a href="https://github.com/IgorIvanter">
+            <a target="_blank" rel="noreferrer" href={LINKEDIN_LINK}>
                 <img src={linkedInIcon} alt=""></img>
             </a>
-            <a href="https://github.com/IgorIvanter">
+            <a target="_blank" rel="noreferrer" href={GITHUB_LINK}>
                 <img src={facebookIcon} alt=""></img>
             </a>
-            <a href="https://github.com/IgorIvanter">
+            <a target="_blank" rel="noreferrer" href={TELEGRAM_LINK}>
                 <img src={instagramIcon} alt=""></img>
             </a>
         </div>
