7057ae53cc65edb0ec5f7426c2cfa84c030eace4
Turing machine <igor.ivanter@gmail.com>
2023-04-23 09:23:33 +0200

    extracted skills data into separate file

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,16 +1,6 @@
 import Carousel from "react-multi-carousel";
-import html5 from "../assets/img/html5.svg"
-import css3 from "../assets/img/css3.svg"
-import js from "../assets/img/js.svg"
-import react from "../assets/img/react.svg"
-import python from "../assets/img/python.svg"
-// import linux from "../assets/img/linux.svg"
-import cpp from "../assets/img/cpp.svg"
-import jquery from "../assets/img/jquery-2.svg"
-import "react-multi-carousel/lib/styles.css";
-import webpack from "../assets/img/webpack.svg"
-import tailwind from "../assets/img/tailwind.svg"
-import wordpress from "../assets/img/wordpress.svg"
+import { skillsData } from '../skills/skillsData';
+
 
 const responsive = {
     superLargeDesktop: {
@@ -33,81 +23,6 @@ const responsive = {
 
 function Skills() {
     const width = 400
-    const skills = [
-        {
-            name: "HTML",
-            src: html5,
-            style: {
-                padding: "5rem"
-            }
-        },
-        {
-            name: "CSS",
-            src: css3,
-            style: {
-                padding: "4rem"
-            }
-        },
-        {
-            name: "JavaScript",
-            src: js,
-            style: {
-                float: "top",
-                padding: "2rem"
-            }
-        },
-        {
-            name: "React",
-            src: react,
-            style: {
-                padding: "5rem"
-            }
-        },
-        {
-            name: "Python",
-            src: python,
-            style: {
-                padding: "6rem"
-            }
-        },
-        {
-            name: "C++",
-            src: cpp,
-            style: {
-                padding: "5rem"
-            }
-        },
-        {
-            name: "jQuery",
-            src: jquery,
-            style: {
-                padding: "11rem 0rem 9.5rem 4rem",
-                width: "400px",
-                height: "400px"
-            }
-        },
-        {
-            name: "Webpack",
-            src: webpack,
-            style: {
-                padding: "6rem 6rem 6rem 6rem"
-            }
-        },
-        {
-            name: "Tailwind CSS",
-            src: tailwind,
-            style: {
-                padding: "5rem"
-            }
-        },
-        {
-            name: "WordPress",
-            src: wordpress,
-            style: {
-                padding: "6rem"
-            }
-        }
-    ]
 
     return (
         <section className="skills" id="skills">
@@ -124,7 +39,7 @@ function Skills() {
                     color: "white"
                 }}
                 infinite={true}>
-                {skills.map(skill => (
+                {skillsData.map(skill => (
                 <div className="logo-box text-center" key={skill.name}>
                     <img src={skill.src} alt="" width={width} style={skill.style} />
                     <h3>
---
+++ src/skills/skillsData.js
@@ -0,0 +1,89 @@
+import html5 from "../assets/img/html5.svg"
+import css3 from "../assets/img/css3.svg"
+import js from "../assets/img/js.svg"
+import react from "../assets/img/react.svg"
+import python from "../assets/img/python.svg"
+// import linux from "../assets/img/linux.svg"
+import cpp from "../assets/img/cpp.svg"
+import jquery from "../assets/img/jquery-2.svg"
+import "react-multi-carousel/lib/styles.css";
+import webpack from "../assets/img/webpack.svg"
+import tailwind from "../assets/img/tailwind.svg"
+import wordpress from "../assets/img/wordpress.svg"
+
+
+export const skillsData = [
+    {
+        name: "HTML",
+        src: html5,
+        style: {
+            padding: "5rem"
+        }
+    },
+    {
+        name: "CSS",
+        src: css3,
+        style: {
+            padding: "4rem"
+        }
+    },
+    {
+        name: "JavaScript",
+        src: js,
+        style: {
+            float: "top",
+            padding: "2rem"
+        }
+    },
+    {
+        name: "React",
+        src: react,
+        style: {
+            padding: "5rem"
+        }
+    },
+    {
+        name: "Python",
+        src: python,
+        style: {
+            padding: "6rem"
+        }
+    },
+    {
+        name: "C++",
+        src: cpp,
+        style: {
+            padding: "5rem"
+        }
+    },
+    {
+        name: "jQuery",
+        src: jquery,
+        style: {
+            padding: "11rem 0rem 9.5rem 4rem",
+            width: "400px",
+            height: "400px"
+        }
+    },
+    {
+        name: "Webpack",
+        src: webpack,
+        style: {
+            padding: "6rem 6rem 6rem 6rem"
+        }
+    },
+    {
+        name: "Tailwind CSS",
+        src: tailwind,
+        style: {
+            padding: "5rem"
+        }
+    },
+    {
+        name: "WordPress",
+        src: wordpress,
+        style: {
+            padding: "6rem"
+        }
+    }
+]
